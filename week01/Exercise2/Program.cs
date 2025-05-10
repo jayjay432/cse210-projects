e38c1f45f3a3cacde39ac32818ff83dd6270a37a
jayjay432 <84500353+jayjay432@users.noreply.github.com>
2025-05-10 08:50:51 +0100

    Adding code to the week1 excersise to get id the student pass a course.

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -5,5 +5,82 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise2 Project.");
+
+        Console.Write("What is your grade percentage? ");
+        string  gradePercentage = Console.ReadLine();
+        int gradePercentageToInt= int.Parse(gradePercentage);
+
+        int passMark = 70;
+        string letterGrade = "";
+
+        if(gradePercentageToInt >= 90)
+        {
+            letterGrade = "A";
+            if(gradePercentageToInt >= passMark)
+            {
+                Console.WriteLine("Congratulations! You've pass this course.");
+            }
+            else
+            {
+                Console.WriteLine("You failed this course.");
+            }
+            Console.WriteLine($"Your grade is {letterGrade}");
+        }
+
+        else if (gradePercentageToInt >= 80)
+        {
+            letterGrade = "B";
+            if(gradePercentageToInt >= passMark)
+            {
+                Console.WriteLine("Congratulations! You've pass this course.");
+            }
+            else
+            {
+                Console.WriteLine("You failed this course.");
+            }
+            Console.WriteLine($"Your grade is {letterGrade}");
+        }
+
+        else if (gradePercentageToInt >=70)
+        {
+            letterGrade = "C";
+            if(gradePercentageToInt >= passMark)
+            {
+                Console.WriteLine("Congratulations! You've pass this course.");
+            }
+            else
+            {
+                Console.WriteLine("You failed this course.");
+            }
+            Console.WriteLine($"Your grade is {letterGrade}");   
+        }
+
+        else if (gradePercentageToInt >= 60)
+        {
+            letterGrade = "D";
+            if(gradePercentageToInt >= passMark)
+            {
+                Console.WriteLine("Congratulations! You've pass this course.");
+            }
+            else
+            {
+                Console.WriteLine("You failed this course.");
+            }
+            Console.WriteLine($"Your grade is {letterGrade}"); 
+        }
+
+        else
+        {
+            letterGrade = "F";
+            if(gradePercentageToInt >= passMark)
+            {
+                Console.WriteLine("Congratulations! You've pass this course.");
+            }
+            else
+            {
+                Console.WriteLine("You failed this course.");
+            }
+            Console.WriteLine($"Your grade is {letterGrade}");
+        }
     }
 }
