068fe947712a01ab734d75adb8d448c414597bce
jayjay432 <84500353+jayjay432@users.noreply.github.com>
2025-05-11 23:44:02 +0100

    Writing functions in c#

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Reflection;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Exercise5 Project.");
+
+        DisplayWelcome();
+        string userName = PromptUserName();
+
+        int favouriteNumber = PromptUserNumber();
+        int square = SquareNumber(favouriteNumber);
+        DisplayResult(square, userName);
+
+
+        static string DisplayWelcome()
+        {
+            Console.WriteLine("Welcome to the Program!");
+            string greeting = Console.ReadLine();
+            return greeting;
+        }
+
+        static string PromptUserName()
+        {
+            Console.Write("Please enter your name: ");
+            string userName = Console.ReadLine();
+            
+            return userName;
+        }
+
+        static int PromptUserNumber()
+        {
+            Console.Write("Please enter your favourite number: ");
+            string answer = Console.ReadLine();
+            int favouriteNumber = int.Parse(answer);
+
+            return favouriteNumber;
+        }
+
+        static int SquareNumber(int favouriteNumber)
+        {
+            int square = favouriteNumber * favouriteNumber;
+           return  square;
+        }
+
+        static void DisplayResult(int square, string userName)
+        {
+            Console.WriteLine($"{userName}, the square of your number is {square}");
+        }
     }
 }
