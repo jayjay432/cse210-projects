1d0fecb909b8da3a098022087f49580618a443dc
jayjay432 <84500353+jayjay432@users.noreply.github.com>
2025-06-19 00:52:07 +0100

    Adding the the excersise program

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -5,5 +5,19 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        List<Activity> activities = new List<Activity>();
+
+        // Create activities
+        activities.Add(new Running(new DateTime(2023, 11, 3), 30, 3.0));
+        activities.Add(new Cycling(new DateTime(2023, 11, 3), 30, 9.7));
+        activities.Add(new Swimming(new DateTime(2023, 11, 3), 30, 20));
+
+        // Display summaries
+        foreach (var activity in activities)
+        {
+            Console.WriteLine(activity.GetSummary());
+        }
+
     }
 }
